30ad4e72554e62fef9cd0a5a0f872da99d19f677
Samantha Salley <samanthasalley@gmail.com>
2017-10-11 02:02:54 -0700

    Add middleware to allow CORS

--- server/server.js
+++ server/server.js
@@ -7,6 +7,21 @@ const bodyParser  = require('body-parser'),
       path        = require('path'),
       routes      = require('./routes');
 
+/**
+ * allowCrossDomain - Sets headers to allow for CORS
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+// Add allowCrossDomain as middleware
+app.use(allowCrossDomain);
 
 /**
  * Serve static files
