c58dc0d652ba3ef41912df099aef5992245de298
Samantha Salley <samanthasalley@gmail.com>
2017-10-11 02:05:55 -0700

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import 'normalize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+      <Switch>
+        <MuiThemeProvider>
+          <Route exact path="/" component={App} />
+        </MuiThemeProvider>
+      </Switch>
+    </BrowserRouter>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
